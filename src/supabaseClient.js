0849ae0fb28061726cd18e6f1f9ad79979498737
Howarf <dwarf0809@naver.com>
2025-11-29 03:42:04 +0900

    check github config in supabaseUrl

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
+console.log("URL 확인:", import.meta.env.VITE_SUPABASE_URL);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 const MODEL_BASE_URL = `${supabaseUrl}/storage/v1/object/public/3dWeb-models/`;
